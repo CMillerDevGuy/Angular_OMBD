7bb2373fa24b160d2297a75ebfc023daf7f694e7
Cody Miller <cmiller@catalystitservices.com>
2015-12-01 15:50:33 -0800

    have a toastr working on the view page

--- src/js/controllers/viewCtrl.js
+++ src/js/controllers/viewCtrl.js
@@ -1,10 +1,20 @@
-angular.module('mainModule').controller('viewCtrl',['$scope', 'movieFactory', 'moreInfo', function($scope, movieFactory, moreInfo){
+angular.module('mainModule').controller('viewCtrl',['$scope', 'movieFactory', 'moreInfo', 'toastr', function($scope, movieFactory, moreInfo, toastr){
     
     $scope.welcomeMsg = "You're in view";
     
     $scope.result = moreInfo;
-    
+    $scope.movieList = movieFactory.getData();
     $scope.addMovie = function(){
-        movieFactory.addMovie(moreInfo);
+        /*for(var i = 0; i < movieList.length; i++){
+            if(movieList[i].data.Title === moreInfo.data.Title){
+                toaster.error("Already in your list");
+            }
+        }*/
+        if(moreInfo.data.Title == null || moreInfo.data.Title == ""){
+            toastr.error('Movie is null','Error');
+        }
+        else{
+            movieFactory.addMovie(moreInfo);
+        }
     }
 }])
